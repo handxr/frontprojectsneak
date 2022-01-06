85ba59ee04fa25a4dfe9366f3feced0fe356c26b
Han <hander.juan@gmail.com>
2022-01-06 19:37:04 +0200

    blur

--- pages/allproducts.js
+++ pages/allproducts.js
@@ -5,9 +5,9 @@ import Link from 'next/link'
 
 
 
-const allproducts = ({products}) => {
-   
-    
+const allproducts = ({ products }) => {
+
+
 
     return (
         <div className="bg-white ">
@@ -15,23 +15,25 @@ const allproducts = ({products}) => {
                 <h2 className="sr-only">Products</h2>
 
                 <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
-                     {products.map((product) => (
+                    {products.map((product) => (
                         <Link href={product.title.slice(" ")} key={product.id}>
-                            <a   className="group ">
+                            <a className="group ">
                                 <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 z-2 ">
-                                     <Image
-                                     priority
+                                    <Image
+                                        placeholder="blur"
+                                        blurDataURL
+                                        priority
                                         src={product.image}
                                         alt="producto"
                                         className="w-full h-full object-center object-cover group-hover:opacity-75 cursor-pointer "
                                         layout="fill"
-                                    /> 
+                                    />
                                 </div>
                                 <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
                                 <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
                             </a>
                         </Link>
-                    ))} 
+                    ))}
                 </div>
             </div>
         </div>
